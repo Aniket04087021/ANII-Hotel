f40996fba0c029a7562061959f92839bda2d745d
ANIKET GUPTA <139427075+Aniket04087021@users.noreply.github.com>
2024-06-28 14:12:06 +0530

    Update Reservation.jsx

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:5006/api/v1/reservation/send",
+        "anii-hotel.vercel.app/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
